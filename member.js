b0136c427b3450e67eb05e3236fda407ccb5b992
Mišel Radosavac <radosavac@mytum.de>
2026-08-26 20:49:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    return {
+        name: "GitHub Skills",
+        role: "Learner",
+        skills: ["JavaScript", "GitHub Copilot", "Codespaces"]
+    };
+}
+
+console.log(skillsMember());
